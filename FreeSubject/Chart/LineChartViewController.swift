e6cb7a946b687c72a51ad3b1d2653e710b2a0180
Hongjunehuke <junhyeok2166@daum.net>
2022-09-17 11:42:41 +0900

    fix : Chart

--- FreeSubject/Chart/LineChartViewController.swift
+++ FreeSubject/Chart/LineChartViewController.swift
@@ -9,17 +9,36 @@ import Foundation
 import UIKit
 import QuartzCore
 import RealmSwift
+import SnapKit
+
+
+struct DataForChart{
+    var Date:Int = 0
+    var inputNum:Int = 0
+}
 
 class LineChartViewController: UIViewController, LineChartDelegate {
 
     var label = UILabel()
     var lineChart: LineChart!
     
+    var ArrayForNum:[DataForChart] = []
+    // createdDate 을 String -> Int 로 변환하고 날짜를 기준으로 오름차순
+    // newArrayForChart <- 정제된 데이터 배열
+    var newArrayForChart:[DataForChart] = []
     
+    lazy var tableView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.74, green: 0.85, blue: 0.78, alpha: 1.00)
+        view.layer.cornerRadius = 16
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         read()
+        view.addSubviews(tableView)
+        
         view.backgroundColor = UIColor(red: 0.94, green: 0.97, blue: 0.95, alpha: 1.0)
         var views: [String: AnyObject] = [:]
         
@@ -27,16 +46,16 @@ class LineChartViewController: UIViewController, LineChartDelegate {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = NSTextAlignment.center
         self.view.addSubview(label)
+
         views["label"] = label
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[label]-|", options: [], metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-80-[label]", options: [], metrics: nil, views: views))
         
         // simple arrays
         let data: [CGFloat] = [3, 4, -2, 11, 13, 15]
-//        let data2: [CGFloat] = [1, 3, 5, 13, 17, 20]
         
         // simple line with custom x axis labels
-        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","hi"]
+        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
         
         lineChart = LineChart()
         lineChart.animation.enabled = true
@@ -47,15 +66,17 @@ class LineChartViewController: UIViewController, LineChartDelegate {
         lineChart.x.labels.values = xLabels
         lineChart.y.labels.visible = true
         lineChart.addLine(data)
-//        lineChart.addLine(data2)
         
         lineChart.translatesAutoresizingMaskIntoConstraints = false
         lineChart.delegate = self
         self.view.addSubview(lineChart)
+
+        
         views["chart"] = lineChart
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[chart]-|", options: [], metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label]-[chart(==200)]", options: [], metrics: nil, views: views))
         
+        setSNP()
 //        var delta: Int64 = 4 * Int64(NSEC_PER_SEC)
 //        var time = dispatch_time(DISPATCH_TIME_NOW, delta)
 //
@@ -73,26 +94,25 @@ class LineChartViewController: UIViewController, LineChartDelegate {
     }
     
     
-    var inputNumForChart:[Int] = []
-    var DateSortResult:[Int] = []
-    
-    struct DataForChart{
-        var Date:Int = 0
-        var inputNum:Int = 0
+    func setSNP(){
+        tableView.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(60)
+            make.leading.equalToSuperview().offset(15)
+            make.trailing.equalToSuperview().offset(-15)
+            make.height.equalTo(300)
+        }
     }
-    
-    var ArrayForNum:[DataForChart] = []
     
     private func read() {
         guard let realm = try? Realm() else { return }
         let models = realm.objects(Day.self)
         for model in models {
-            inputNumForChart += [model.iconFeeling]
             let mdl = DataForChart(Date: Int(model.createdDate) ?? 000000, inputNum: model.iconFeeling)
             ArrayForNum += [mdl]
         }
         // createdDate 을 String -> Int 로 변환하고 날짜를 기준으로 오름차순
-        let newArrayForChart = ArrayForNum.sorted(by:{$0.Date < $1.Date})
+        // newArrayForChart - 이게 정제된 데이터 배열
+        self.newArrayForChart = ArrayForNum.sorted(by:{$0.Date < $1.Date})
         print(newArrayForChart)
     }
     
