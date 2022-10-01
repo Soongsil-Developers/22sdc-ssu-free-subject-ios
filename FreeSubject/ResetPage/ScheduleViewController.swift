96e71ac9b7dbd04a6c08eaee8763c5eb2f4f884f
Hongjunehuke <junhyeok2166@daum.net>
2022-10-01 19:39:54 +0900

    fix:modal & resource:ScheduleView

---
+++ FreeSubject/ResetPage/ScheduleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleViewController.swift
+//  FreeSubject
+//
+//  Created by 홍준혁 on 2022/10/01.
+
+import UIKit
+import SnapKit
+import Then
+import RealmSwift
+import Realm
+
+class ScheduleViewController : UIViewController{
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+    }
+}
